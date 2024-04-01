8c843bb5f0a870c3d3b8c9b7bd76f605f6f2aa50
langtu262 <trungkienit87@gmail.com>
2024-04-01 15:53:23 +0700

    1

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
-    [SerializeField] private float jumFore = 10.0f;
+    [SerializeField] private float jumFore = 5.0f;
     [SerializeField] Transform groundCheck;
     public LayerMask groundLayer;
 
@@ -23,14 +24,36 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        isGround = Physics2D.OverlapCircle(groundCheck.position,0.2f,groundLayer);
         if(horizontalInput != 0 )
         {
             Move(horizontalInput);
         }
+        if(isGround&& Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+              
     }
+
     private void Move(float dir)
     {
         rb.velocity = new Vector2 (dir * moveSpeed, rb.velocity.y);
+        if( dir > 0 && !facingRight || dir < 0 &&facingRight)
+       
+        {
+            Flip();
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector2 (rb.velocity.x, jumFore);
+    }
+    void Flip()
+    {
+        facingRight =!facingRight;
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
