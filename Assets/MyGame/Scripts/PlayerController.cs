0bc61280966a7a8d7c2208c8223612f20006764b
langtu262 <trungkienit87@gmail.com>
2024-04-02 12:48:57 +0700

    1

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
-    [SerializeField] private float jumFore = 5.0f;
+    [SerializeField] private float jumFore = 3.0f;
     [SerializeField] Transform groundCheck;
     public LayerMask groundLayer;
 
@@ -14,10 +14,15 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
     private bool isGround;
     private bool facingRight = true;
+    private int idRunning;
+    private int idJump;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        idRunning = Animator.StringToHash("isRunning");
+        idJump = Animator.StringToHash("isJump");
     }
 
     // Update is called once per frame
@@ -29,25 +34,40 @@ public class PlayerController : MonoBehaviour
         {
             Move(horizontalInput);
         }
-        if(isGround&& Input.GetKeyDown(KeyCode.Space))
+        else
+        {
+            anim.SetBool(idRunning, false);
+        }
+
+        if(isGround&&Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
-              
+        if(isGround == false)
+        {
+            anim.SetBool(idRunning, false);
+            anim.SetBool(idJump, true);
+        }
+        else
+        {
+            anim.SetBool(idJump, false);
+        }
+
     }
 
     private void Move(float dir)
     {
         rb.velocity = new Vector2 (dir * moveSpeed, rb.velocity.y);
-        if( dir > 0 && !facingRight || dir < 0 &&facingRight)
-       
+        if (dir > 0 && !facingRight || dir < 0 && facingRight)
         {
             Flip();
         }
+        anim.SetBool(idRunning, true);
     }
     private void Jump()
     {
         rb.velocity = new Vector2 (rb.velocity.x, jumFore);
+        
     }
     void Flip()
     {
