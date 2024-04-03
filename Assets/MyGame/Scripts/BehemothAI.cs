56dac4f2732a25f59eaaef8ede051d650dafa45a
langtu262 <trungkienit87@gmail.com>
2024-04-03 16:10:06 +0700

    1

--- Assets/MyGame/Scripts/BehemothAI.cs
+++ Assets/MyGame/Scripts/BehemothAI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BehemothAI : EnemyAI
@@ -11,35 +12,66 @@ public class BehemothAI : EnemyAI
     [SerializeField] private float speed;
     [SerializeField] private SpriteRenderer sp;
     [SerializeField] private Animator anim;
+    private Transform target;
+    [SerializeField] private float speedMove = 3;
+    [SerializeField] private float stopDistance = 3;
+    private Vector3 pointC;
     private Vector3 targetPos;
     private int isIdle;
-
+    [SerializeField] private float attackDistance;
     void Start()
     {
-        
-        transform.position = pointA.position;
+
+        pointC = pointA.position;
         isIdle = Animator.StringToHash("isIdle");
+        target=GameObject.FindGameObjectWithTag("Player").transform;
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-            return;*/
-        if (transform.position==pointA.position) 
+        if (target != null)
         {
-            targetPos = pointB.position;
-            sp.flipX = false;
-            anim.SetTrigger(isIdle);
+           
+            Vector2 dir = (target.position - transform.position).normalized;
+            if (Vector2.Distance(transform.position,target.position) < stopDistance)
+            {
+                rb.velocity = dir* speedMove;
 
-        }
-        else if(transform.position==pointB.position)
-        {
-            targetPos = pointA.position;
-            sp.flipX = true;
-            anim.SetTrigger(isIdle);
-        }
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, target.position) < attackDistance)
+                {
+                    anim.SetBool("isAttack", true);
+                }
+                else
+                {
+                    anim.SetBool("isAttack", false);
+                }
+            }
+            else if(Vector2.Distance(transform.position, target.position) > stopDistance) 
+            {
+                /*if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+                     return;*/
+                if (pointC == pointA.position)
+                {
+                    targetPos = pointB.position;
+                    sp.flipX = false;
+                    anim.SetTrigger(isIdle);
+                    pointC = pointB.position;
+                }
+                if (transform.position==pointB.position)
+                {
+                    targetPos = pointA.position;
+                    sp.flipX = true;
+                    anim.SetTrigger(isIdle);
+                }
+                if (transform.position == pointA.position)
+                {
+                    pointC = pointA.position;                   
+                }                      
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            }
+        }        
     }
+
 }
